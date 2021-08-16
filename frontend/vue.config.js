e93f263d6c792f7ec3d76b1a952bb502fabb95b3
ertogrul <michalkordyzon@gmail.com>
2021-08-16 09:31:03 +0200

    daily

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,17 +1,12 @@
 module.exports = {
-  // devServer: {
-  //   port: 8081,
-  //   proxy: "<url>"
-  // },
-  chainWebpack: config => {
-    config.module
-      .rule('vue')
-      .use('vue-loader')
-        .loader('vue-loader')
-        .tap(options => {
-          options.prettify = false
-          // options.hotReload = false // disables Hot Reload // TODO, isn't working here
-          return options
-        })
-  }
+    chainWebpack: config => {
+        config.module
+            .rule('vue')
+            .use('vue-loader')
+            .loader('vue-loader')
+            .tap(options => {
+                options.prettify = false
+                return options
+            })
+    }
 }
